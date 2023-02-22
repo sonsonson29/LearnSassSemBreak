8eb96ee1199c94e0062900943857fef65cc97d18
kambal <Kambal_account@Aprils-MacBook-Air.local>
2023-02-22 14:15:52 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return src('shinobi/**/*.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  watch(['shinobi/**/*.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
+
+// shinobi/**/* -> this will search any scss file to inside the directory
